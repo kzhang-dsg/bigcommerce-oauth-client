5d0a87cb0401b8cc9e54be31270a46bdfbfd7ba7
kzhang <kzhang@dunnsolutions.com>
2023-03-18 10:53:51 -0500

    0.2.0 release

---
+++ lib/index.d.ts
@@ -0,0 +1,11 @@
+import { AxiosInstance } from "axios";
+import { AccessToken, CustomerJwt, OAuthCode, Options, StoreAdminJwt } from "./model/oauth";
+export declare class BigCommerceOAuthClient {
+    private readonly options;
+    readonly axiosInstance: AxiosInstance;
+    constructor(options: Options);
+    authorize<T extends OAuthCode, R extends AccessToken>(oauthCode: T, redirectUri: string): Promise<R>;
+    verifyStoreAdminJwt(token: string): StoreAdminJwt;
+    verifyCustomerJwt(token: string): CustomerJwt;
+    createCustomerLoginJwt(customerId: number, redirectUrl?: string, channelId?: number): string;
+}
